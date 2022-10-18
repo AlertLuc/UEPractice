c50272d4d09ae89b98da500346237cf3279c6207
Luc <2235615010@qq.com>
2022-10-18 23:24:16 +0800

    Import static grid resources and bone grid resources

--- Scripts/AssetFunctions.py
+++ Scripts/AssetFunctions.py
@@ -2,59 +2,82 @@ import unreal
 import os
 
 
+# Create an import resource task
+def buildImportTask(filename, destination_path, options=None):
+    task = unreal.AssetImportTask()
+    task.set_editor_property('automated', True)
+    task.set_editor_property('destination_name', '')
+    task.set_editor_property('destination_path', destination_path)
+    task.set_editor_property('filename', filename)
+    task.set_editor_property('replace_existing', True)
+    task.set_editor_property('save', True)
+    task.set_editor_property('options', options)
+    return task
 
-'''
-absolute path,not relative path
-'''
-texture_tga = 'D:/WorkPlace/FirstPersonCrosshair.TGA' 
-sound_wav = 'D:/WorkPlace/FirstPersonTemplateWeaponFire02.WAV' 
 
-'''
-Create an import resource task
-filename:Create the name of the import
-destination_path:Which path to import to UE4
-Did not perform
-'''
-def buildImportTask(filename, destination_path):
-	'''
-	Create a task object
-    Set the properties
-    Avoid dialog boxes
-    Name
-	Path
-    Filename
-    Whether the override already exists
-    Whether or not to save
-	'''
-	task = unreal.AssetImportTask()
-	task.set_editor_property('automated', True)
-	task.set_editor_property('destination_name','')
-	task.set_editor_property('destination_path', destination_path)
-	task.set_editor_property('filename', filename)
-	task.set_editor_property('replace_existing', True)
-	task.set_editor_property('save', True)
-	return task
+def buildStaticMeshImportOptions():
+    options = unreal.FbxImportUI()
 
-'''
-Example Import resources
-The print information
-'''
+    options.set_editor_property('import_mesh', True)
+    options.set_editor_property('import_textures', False)
+    options.set_editor_property('import_materials', True)
+    options.set_editor_property('import_as_skeletal', False)
+
+
+    options.static_mesh_import_data.set_editor_property('import_translation', unreal.Vector(50.0, 0.0, 0.0))
+    options.static_mesh_import_data.set_editor_property('import_rotation', unreal.Rotator(0.0, 110.0, 0.0))
+    options.static_mesh_import_data.set_editor_property('import_uniform_scale', 1.0)
+
+    options.static_mesh_import_data.set_editor_property('combine_meshes', True)
+    options.static_mesh_import_data.set_editor_property('generate_lightmap_u_vs', True)
+    options.static_mesh_import_data.set_editor_property('auto_generate_collision', True)
+
+    return options
+
+
+def buildskeletalMeshImportOptions():
+    options = unreal.FbxImportUI()
+    options.set_editor_property('import_mesh', True)
+    options.set_editor_property('import_textures', False)
+    options.set_editor_property('import_materials', True)
+    options.set_editor_property('import_as_skeletal', True)
+
+    options.static_mesh_import_data.set_editor_property('import_translation', unreal.Vector(0.0, 0.0, 0.0))
+    options.static_mesh_import_data.set_editor_property('import_rotation', unreal.Rotator(0.0, 110.0, 0.0))
+    options.static_mesh_import_data.set_editor_property('import_uniform_scale', 1.0)
+
+    return options
+
+
+# Example Import resources
 def executeImportTasks(tasks):
-	unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks(tasks)
-	for task in tasks:
-		for path in task.get_editor_property('imported_object_paths'):
-			print ('Imported: %s' % path)
-
-'''
-Formal transfer
-Perform the Create picture task and the audio task
-Pass the list in executeImportTasks
-'''
-def importMyAssets():
-	texture_task = buildImportTask(texture_tga, '/FirstPerson/Texture')
-	sound_task = buildImportTask(sound_wav, '/FirstPerson/Audio')
-	executeImportTasks([texture_task, sound_task])
+    unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks(tasks)
+    for task in tasks:
+        for path in task.get_editor_property('imported_object_paths'):
+            print('Imported: %s' % path)
 
 
+# absolute path,not relative path
+def importSimpleAssets():
+    texture_tga = 'D:/WorkPlace/Scripts/FirstPersonCrosshair.TGA'
+    sound_wav = 'D:/WorkPlace/Scripts/FirstPersonTemplateWeaponFire02.WAV'
+
+    texture_task = buildImportTask(texture_tga, '/Game/Texture')
+    sound_task = buildImportTask(sound_wav, '/Game/Audio')
+    executeImportTasks([texture_task, sound_task])
 
 
+def importMeshAssets():
+    static_mesh_path = 'D:/WorkPlace/Scripts/SM_Chair.FBX'
+    skeletal_mesh_path = 'D:/WorkPlace/Scripts/SK_Mannequin.FBX'
+
+    static_mesh_task = buildImportTask(static_mesh_path, '/Game/StaticMeshes', buildStaticMeshImportOptions())
+    skeletal_mesh_task = buildImportTask(skeletal_mesh_path, '/Game/SkeletalMeshes', buildskeletalMeshImportOptions())
+
+    executeImportTasks([static_mesh_task, skeletal_mesh_task])
+
+
+# Formal transfer
+def importAssets():
+    # importSimpleAssets()
+    importMeshAssets()
